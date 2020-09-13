1e9e658b04d9005493dcbdc476ced26cd73f5c78
MarcelodeFreitas <55439529+MarcelodeFreitas@users.noreply.github.com>
2020-09-13 17:19:26 +0100

    commit

--- exercicios-web/controle/breakContinue.js
+++ exercicios-web/controle/breakContinue.js
@@ -1,15 +1,17 @@
 const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
+//imprime ate ao indice 4
 for (x in nums) {
     if (x == 5) {
-        break
+        break //direciona para fora do bloco for (da estrutura de controle mais proxima)
     }
     console.log(`${x} = ${nums[x]}`)
 }
 
+//avança o indice 5
 for (y in nums) {
     if (y == 5) {
-        continue
+        continue //interrompe a execuçao atual e volta a entrar no laço
     }
     console.log(`${y} = ${nums[y]}`)
 }
@@ -17,7 +19,7 @@ for (y in nums) {
 externo:
 for (a in nums) {
     for (b in nums) {
-        if(a == 2 && b == 3) break externo
+        if(a == 2 && b == 3) break externo //break rotulado: vai para o for mais externo
         console.log(`Par = ${a},${b}`)
     }
 }
